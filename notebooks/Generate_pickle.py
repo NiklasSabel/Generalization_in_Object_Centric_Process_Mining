40b0675b1e2490587dc59824869d42dc1825a9aa
NiklasSabel <67095971+NiklasSabel@users.noreply.github.com>
2023-06-21 13:30:01 +0200

    Notebooks Update

--- notebooks/Generate_pickle.py
+++ notebooks/Generate_pickle.py
@@ -10,31 +10,32 @@ from src.utils import get_happy_path_log, create_flower_model, generate_variant_
 
 print('Load file')
 
-filename = "../src/data/jsonocel/BPI2017-Final.jsonocel"
+filename = "../src/data/jsonocel/DS4.jsonocel"
 ocel = ocel_import_factory.apply(filename)
 ocpn = ocpn_discovery_factory.apply(ocel, parameters={"debug": False})
 
-with open("../src/data/csv/BPI_log.pickle", 'wb') as fp:
+with open("../src/data/csv/DS4_log.pickle", 'wb') as fp:
 	pickle.dump(ocel, fp)
 
-with open("../src/data/csv/BPI_ocpn.pickle", 'wb') as fp:
+with open("../src/data/csv/DS4_ocpn.pickle", 'wb') as fp:
 	pickle.dump(ocpn, fp)
 
+print('Save file happy')
 
 happy_path__ocel = get_happy_path_log(filename)
 
 happy_path_ocpn = ocpn_discovery_factory.apply(happy_path__ocel, parameters={"debug": False})
 
-print('Save file happy')
 
 
-with open("../src/data/csv/bpi_ocpn_happy.pickle", 'wb') as fp:
+
+with open("../src/data/csv/DS4_ocpn_happy.pickle", 'wb') as fp:
 	pickle.dump(happy_path_ocpn, fp)
 
 print('Save file flower')
 
-ots = ["application","offer"]
+ots = ["Payment application","Control summary","Entitlement application","Geo parcel document","Inspection","Reference alignment"]
 flower_ocpn = create_flower_model(filename,ots)
 
-with open("../src/data/csv/bpi_ocpn_flower.pickle", 'wb') as fp:
-	pickle.dump(happy_path_ocpn, fp)
+with open("../src/data/csv/DS4_ocpn_flower.pickle", 'wb') as fp:
+	pickle.dump(flower_ocpn, fp)
